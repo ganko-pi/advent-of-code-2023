9d4a38ffb2ec031e5f56706f9a29ab61839fbc91
Ganko Pi <ganko-pi@outlook.de>
2023-12-09 16:59:46 +0100

    feat: code for puzzle 2 of day 7

---
+++ Day7-2/HandWithBid.cs
@@ -0,0 +1,3 @@
+namespace Day7_1;
+
+public record HandWithBid(Hand Hand, int Bid);
---
+++ Day7-2/Type.cs
@@ -0,0 +1,12 @@
+namespace Day7_1;
+
+public enum Type
+{
+    HIGH_CARD,
+    ONE_PAIR,
+    TWO_PAIR,
+    THREE_OF_A_KIND,
+    FULL_HOUSE,
+    FOUR_OF_A_KIND,
+    FIVE_OF_A_KIND,
+}
